0198996ca00d74555dd20a0e32a47d54b726224a
kouta-wolf <koutaookami18@gmail.com>
2026-05-10 11:58:44 +0900

    feat: RSpecを用いた税率計算のロジックを作成

---
+++ 2026/5-10-first-programming/calculator.rb
@@ -0,0 +1,19 @@
+class PriceCalculator
+  TAX_RATES = { normal: 0.10, reduced: 0.08 }
+
+  def self.calculate(items)
+    sub_total = 0
+    tax_total = 0
+
+    items.each do |item|
+      rate = TAX_RATES[item[:category]]
+      sub_total += item[:price]
+      tax_total += (item[:price] * rate).floor
+    end
+
+    total_with_tax = sub_total + tax_total
+    total_with_tax -= 500 if total_with_tax >= 5000
+
+    { total: total_with_tax }
+  end
+end
