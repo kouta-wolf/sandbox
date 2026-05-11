ec229a96e05c913ac247e481f3a5ab1cef0ce527
kouta-wolf <koutaookami18@gmail.com>
2026-05-11 16:11:36 +0900

    refactor: main.rbを見やすい形に変更

--- 2026/05-11-user-registration-validator/main.rb
+++ 2026/05-11-user-registration-validator/main.rb
@@ -3,35 +3,17 @@ class UserValidator
     errors = []
 
     # ユーザー名のバリデーション
-    if username.length < 3
-      errors << "ユーザー名は3文字以上で入力してください"
-    end
+    errors << "ユーザー名は3文字以上で入力してください" if username.length < 3
+    errors << "ユーザー名は15文字以内で入力してください" if username.length > 15
+    errors << "ユーザー名は半角英数字で入力してください" if username.match?(/[^a-zA-Z0-9]/)
 
-    if username.length > 15
-      errors << "ユーザー名は15文字以内で入力してください"
-    end
-
-    if username.match?(/[^a-zA-Z0-9]/)
-      errors << "ユーザー名は半角英数字で入力してください"
-    end
-
-    #パスワードのチェック
-    if password.length < 8
-      errors << "パスワードは8文字以上で入力してください"
-    end
-
-    if password.count("A-Z") == 0
-      errors << "パスワードは英大文字を含めて入力してください"
-    end
-
-    if password.count("a-z") == 0
-      errors << "パスワードは英小文字を含めて入力してください"
-    end
-
-    if password.count("0-9") == 0
-      errors << "パスワードは数字を含めて入力してください"
-    end
+    # パスワードのチェック
+    errors << "パスワードは8文字以上で入力してください" if password.length < 8
+    errors << "パスワードは英大文字を含めて入力してください" if password.count("A-Z") == 0
+    errors << "パスワードは英小文字を含めて入力してください"   if password.count("a-z") == 0
+    errors << "パスワードは数字を含めて入力してください" if password.count("0-9") == 0
     
-    { valid: errors.empty?, errors: errors } # valid: true, errors: [] の形か valid: false, errors: ["hoge"]の形になる
+    # 出力
+    { valid: errors.empty?, errors: errors } # valid: true, errors: []
   end
 end
