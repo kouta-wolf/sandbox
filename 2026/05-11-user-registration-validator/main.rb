8fe1c986c48493e30e8f18465dc74807328c4da3
kouta-wolf <koutaookami18@gmail.com>
2026-05-13 15:08:50 +0900

    refactor: ロジックが長いため修正

--- 2026/05-11-user-registration-validator/main.rb
+++ 2026/05-11-user-registration-validator/main.rb
@@ -5,18 +5,25 @@ class UserValidator
   def self.validate(username, password)
     errors = []
 
+    validate_username(username, errors)
+    validate_password(password, errors)
+
+    # 出力
+    { valid: errors.empty?, errors: errors } # valid: true, errors: []
+  end
+
+  private_class_method def self.validate_username(username, errors)
     # ユーザー名のバリデーション
     errors << 'ユーザー名は3文字以上で入力してください' if username.length < 3
     errors << 'ユーザー名は15文字以内で入力してください' if username.length > 15
     errors << 'ユーザー名は半角英数字で入力してください' if username.match?(/[^a-zA-Z0-9]/)
+  end
 
+  private_class_method def self.validate_password(password, errors)
     # パスワードのチェック
     errors << 'パスワードは8文字以上で入力してください' if password.length < 8
     errors << 'パスワードは英大文字を含めて入力してください' if password.count('A-Z').zero?
     errors << 'パスワードは英小文字を含めて入力してください' if password.count('a-z').zero?
     errors << 'パスワードは数字を含めて入力してください' if password.count('0-9').zero?
-
-    # 出力
-    { valid: errors.empty?, errors: errors } # valid: true, errors: []
   end
 end
