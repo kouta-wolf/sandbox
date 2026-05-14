2bbd59eb03f87c5abc0ca7287a5d6e2f9f0f0603
kouta-wolf <koutaookami18@gmail.com>
2026-05-14 16:06:58 +0900

    初期ファイル作成 #6

---
+++ 2026/05-13-react-user-json/json.rb
@@ -0,0 +1,12 @@
+require "json"
+require_relative "data_generator"
+
+# config
+NUMBER = 50
+
+users = DataGenerator.generate_users(NUMBER)
+file_path = File.join(__dir__, 'users.json')
+
+File.write(file_path, JSON.pretty_generate(users))
+
+puts "Successfully created: #{file_path}"
