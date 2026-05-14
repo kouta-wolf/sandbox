6e7bbb1537e63d5f24606f428cfeff9bfd8bb4ca
kouta-wolf <koutaookami18@gmail.com>
2026-05-14 16:19:08 +0900

    feat: トップページにjsonデータを反映できるように作成

--- 2026/05-13-react-user-json/web-app/src/App.jsx
+++ 2026/05-13-react-user-json/web-app/src/App.jsx
@@ -1,10 +1,11 @@
-function App() {
+import usersData from '../../users.json';
+
+export default function App() {
   return (
     <div>
       <h1>User Dashboard</h1>
       <p>ここにRubyで作ったデータを表示します</p>
+      <p>読み込んだユーザー数: {usersData.length}人</p>
     </div>
   );
 }
-
-export default App
