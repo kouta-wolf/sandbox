cbf2205299e2ebb7c35f87e45e6ca6d1cf835d64
kouta-wolf <koutaookami18@gmail.com>
2026-06-05 13:02:23 +0900

    オブジェクトの確認

--- 2026/06-04-ruby_foundation/main.rb
+++ 2026/06-04-ruby_foundation/main.rb
@@ -6,7 +6,21 @@ p 1.class # => Integer
 p Integer.is_a?(Object) # => true
 p Integer.class # => Class
 
-# putsはkernelモジュールから呼び出される
-p method(:puts) # => #<Method: Object(Kernel)#puts(*)>
+# Class.classもオブジェクトであり、ClassクラスはClassクラスを知っている
+p Class.is_a?(Object) # => true
+p Class.class # => Class
 
+# オブジェクトは遡るといずれKernelにたどりつく
+p Class.ancestors # => [Class, Module, Object, Kernel, BasicObject]
+
+# putsはmainオブジェクトのkernelモジュールから呼び出される
+p method(:puts) # => #<Method: Object(Kernel)#puts(*)>
 p method(:puts).parameters # => [[:rest]]  残りの引数すべてという意味でputsは可変長引数
+
+# メソッドはオブジェクトではないが、オブジェクトに変換できる
+p method(:puts).is_a?(Object) # => true
+p method(:puts).class # => Method
+p Method.class # => Class
+
+
+
